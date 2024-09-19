a830e22ec6387af96747b594a5c4750b57e372a6
unknown <poomagal@digitalseo.in>
2024-09-19 10:46:16 +0300

    prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -46,9 +46,10 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
         logging.info("Error occured in utils")
         raise CustomException(e,sys)
     
-    
-    mae=mean_absolute_error(true, predicted)
-    mse=mean_squared_error(true, predicted)
-    rmse=np.sqrt(mean_squared_error(true, predicted))
-    r2_square=r2_score(true, predicted)
-    return mae, rmse, r2_square
+def load_object(file_path):
+        try:
+            with open(file_path, 'rb') as file_obj:
+                return pickle.load(file_obj)
+        except Exception as e:
+            logging.info("Exception occurs in load_object function")
+            raise CustomException(e, sys)
